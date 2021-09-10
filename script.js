65e18f4356d054d993c6fed4bbcb90a5f7d4eb74
Borvu400 <Borvu400@gmail.com>
2021-09-10 08:31:15 +0000

    bal

--- script.js
+++ script.js
@@ -19,8 +19,55 @@ function setup() {
  */
 function draw() {
   // stel vulkleur in
-  fill(100, 100, 255);
+  
 
   // teken een cirkel
+  fill(100, 255, 100);
   ellipse(50,50,80,80);
+
+  fill(255, 100, 100);
+  ellipse(250,50,80,80);
+
+  fill(90, 60, 30);
+  ellipse(450,50,80,80);
+
+  fill(30, 60, 90);
+  ellipse(650,50,80,80);
+
+  fill(60, 90, 30);
+  ellipse(850,50,80,80);
+
+  // Bal 1
+  if(x [0]>1230){
+    SpeedX = SpeedX * -1
+  }
+
+  if(x [0]<0){
+    SpeedX = SpeedX * -1
+  }
+
+  if(y[0]>670){
+    SpeedY = SpeedY * -1
+  }
+
+  if(y [0]<0){
+    SpeedY = SpeedY * -1
+  }
+
+  //bal 2
+  if(x [1]>1230){
+    SpeedX = SpeedX * -1
+  }
+
+  if(x [1]<0){
+    SpeedX = SpeedX * -1
+  }
+
+  if(y[1]>670){
+    SpeedY = SpeedY * -1
+  }
+
+  if(y [1]<0){
+    SpeedY = SpeedY * -1
+  }
 }
